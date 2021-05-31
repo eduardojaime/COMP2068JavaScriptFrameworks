f953bec304bc44f2bffc9827835c0d01892726c0
Eduardo Jaime <eduardojaime86@gmail.com>
2021-05-31 19:15:30 -0400

    Code for lesson 3

---
+++ lesson03/httpmodule.js
@@ -0,0 +1,14 @@
+// Example: https://www.w3schools.com/nodejs/nodejs_http.asp
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+    res.write(req.url); // Read url from requests
+    // maybe return different content based on URL
+
+    // Default response
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!'); //write a response to the client
+
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080 
