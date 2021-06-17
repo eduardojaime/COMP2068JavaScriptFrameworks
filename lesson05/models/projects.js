95a5dda7e3d9b2b71ab9d0e80458e52e33304dae
Eduardo Jaime <eduardojaime86@gmail.com>
2021-06-17 07:49:09 -0400

    Update projects.js

--- lesson05/models/projects.js
+++ lesson05/models/projects.js
@@ -24,6 +24,6 @@ const projectsSchemaDefinition = {
 // Create new mongoose schema using the definition object
 var projectsSchema = new mongoose.Schema(projectsSchemaDefinition);
 // Create new mongoose model using the schema object and
-// Import new model
+// Import new model > provide name and schema
 module.exports = mongoose.model('Project', projectsSchema);;
 // alternative > module.exports = mongoose.model('Project', projectsSchema);
