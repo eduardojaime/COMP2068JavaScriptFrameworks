fbc268bca49dec3f490fcdcdcc88774fe4bdc656
Eduardo Jaime <eduardojaime86@gmail.com>
2021-07-15 17:31:02 -0400

    Added globals file

--- lesson08/config/globals.js
+++ lesson08/config/globals.js
@@ -1,9 +1,9 @@
 const configurations = {
     'db': 'mongodb+srv://admin:<password>@cluster0.86msx.mongodb.net/comp2068',
     'github': {
-        'clientId': '',
-        'clientSecret':'',
-        'callbackUrl':'http://localhost:3000/github/callback'
+        'clientId': '548e998eb1da351f9819',
+        'clientSecret': 'a1928f6e4223999b30335e533a429929d6f461aa',
+        'callbackUrl': 'http://localhost:3000/github/callback'
     }
 }
 
