fa056d37e2d4d760846ada89bbeaeadae5393daa
Eduardo Jaime <eduardojaime86@gmail.com>
2021-07-23 17:18:36 -0400

    Part 4

--- demos/lesson09demoB/my-app/src/app/app.module.ts
+++ demos/lesson09demoB/my-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 // components
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProjectComponent } from './project/project.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [ProjectComponent]
--- demos/lesson09demoB/my-app/src/app/project/project.component.ts
+++ demos/lesson09demoB/my-app/src/app/project/project.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+// create an in-memory model class of a project
+// to mock up some data
+export class Project {
+  id!: number;
+  name!: string;
+  course!: string;
+}
+
+/// [] means list
+// create constant called PROJECTS as list of project
+// constant all caps
+const PROJECTS: Project[] = [
+  { id: 1, name: 'Lab 1', course: 'JS' },
+  { id: 2, name: 'Lab 2', course: 'JS' },
+  { id: 3, name: 'Lab 3', course: 'JS' },
+  { id: 4, name: 'Tutorials', course: 'JS' }
+];
+
 @Component({
   selector: 'app-project', // <app-project>
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
+
 export class ProjectComponent implements OnInit {
 
+  // add fields to the class
+  // project: Project = { id: 1, name: 'Lab 3', course: 'JS' };
+  projects = PROJECTS;
+  // create field to store selected project object
+  // selectedProject: Project = { id: 0, name: '', course: '' };
+  selectedProject!: Project;
+  
   constructor() { }
-
+  // component class can have methods 
   ngOnInit(): void {
+
+  }
+
+  onSelect(project: Project): void {
+    this.selectedProject = project;
   }
 
 }
