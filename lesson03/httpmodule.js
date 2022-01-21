bdfc8dea14d7a55a777e935f37b85a3c4a6a3760
Eduardo Jaime <eduardojaime86@gmail.com>
2022-01-20 20:07:40 -0500

    Fixes

--- lesson03/httpmodule.js
+++ lesson03/httpmodule.js
@@ -8,7 +8,7 @@ http.createServer(function (req, res) {
     // maybe return different content based on URL
 
     // Default response
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Hello World!'); //write a response to the client
 
     res.end(); //end the response
