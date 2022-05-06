30e3249b2139a6b44be24642194ed8bf807cf345
Eduardo Jaime <eduardojaime86@gmail.com>
2022-05-06 19:00:28 -0400

    Archived old demos

---
+++ archived/W22/SPA/server/config/globals.js
@@ -0,0 +1,6 @@
+const configurations = {
+    'db': 'mongodb+srv://admin:strongpassword2022@cluster0.86msx.mongodb.net/projecttracker',
+    'clientUrl': 'http://localhost:4200'
+}
+
+module.exports = configurations;
