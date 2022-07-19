979ec2b075354cdd9f01f181a48166d7dba45c4d
Eduardo Jaime <eduardojaime86@gmail.com>
2022-07-19 19:13:11 -0400

    L10 part 2 building the angular frontend client

---
+++ demosA/lesson10/project-spa-frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+// equivalent to app.js
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [ProjectComponent]
+})
+export class AppModule { }
