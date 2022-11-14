b791921fe5b4d3a97c50ffc5c3127ef5797c7f0b
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-14 18:56:28 -0500

    l09 part 3

--- demos/lesson09/my-app/src/app/app.module.ts
+++ demos/lesson09/my-app/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProjectComponent]
 })
 export class AppModule { }
