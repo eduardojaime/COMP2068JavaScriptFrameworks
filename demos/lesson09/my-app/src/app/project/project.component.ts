22c0bf349fe408b886da8591d2fd7ff322061578
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-14 19:02:23 -0500

    l09 part 3 mock data

--- demos/lesson09/my-app/src/app/project/project.component.ts
+++ demos/lesson09/my-app/src/app/project/project.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+// class to represent a project
+export class Project {
+  id!:number;
+  name!:string;
+  course!:string;
+}
+
+// mock a list of Project
+const PROJECTS: Project[] = [
+  {id:101, name:'Lab1', course:'JS Frameworks'},
+  {id:102, name:'Lab2', course:'JS Frameworks'},
+  {id:103, name:'Lab3', course:'JS Frameworks'}
+];
+
 
 @Component({
   selector: 'app-project',
@@ -6,6 +20,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
+  projects = PROJECTS;
 
   constructor() { }
 
