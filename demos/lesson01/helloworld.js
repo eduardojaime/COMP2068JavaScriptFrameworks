36a8c88e80403fcaa2d80fad7a5ee5d27c105528
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-12 21:50:26 -0500

    l01 demo files

---
+++ demos/lesson01/helloworld.js
@@ -0,0 +1,7 @@
+// Print a message to the console
+console.log('Hello world!');
+// you can have as many lines of code and functions as you need
+
+// Run from the terminal
+// node <filename> without extension
+// > node helloworld
