15c931be8310e794d96c99347365b7cc9e4274b3
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-19 21:25:51 -0500

    l02 part 2 closures

---
+++ demos/lesson02/2-closures/openFunction.js
@@ -0,0 +1,10 @@
+// this is not a closure, just a regular function
+function openFunction() {
+    let counter = 1;
+    counter++; // 2
+    console.log(counter); 
+}
+
+// call twice and guess the output
+openFunction(); // 2
+openFunction(); // 2
