737f91bbf9b5102e5f12bd7b879ec415d5272b16
Eduardo Jaime <eduardojaime86@gmail.com>
2023-02-09 21:47:11 -0500

    l05 part 4 adding a model and creating projects

--- demos/lesson05/routes/projects.js
+++ demos/lesson05/routes/projects.js
@@ -2,12 +2,35 @@
 const express = require("express");
 // create router object
 const router = express.Router();
+// import Project model
+const Project = require("../models/project"); // use .. to navigate one folder level up
 // configure router object
 // GET /projects/
 router.get("/", (req, res, next) => {
   res.render("projects/index", { title: "Project Tracker" });
 });
 // GET /projects/add
+router.get("/add", (req, res, next) => {
+  res.render("projects/add", { title: "Add a new Project" });
+});
+// POST /projects/add
+router.post("/add", (req, res, next) => {
+  // need to use mongoose model
+  // data coming from request body (input fields)
+  Project.create(
+    { // new project data in JSON format
+      name: req.body.name,
+      dueDate: req.body.dueDate,
+      course: req.body.course,
+    },
+    (err, newProject) => { // callback function to execute after processing is complete
+        if (err) { console.log(err); }
+        else {
+            res.redirect('/projects');
+        }
+    }
+  );
+});
 
 // export router object
 module.exports = router;
