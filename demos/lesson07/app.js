efb5d7dcbc6346592f2ff443c98161dc7a25c758
Eduardo Jaime <eduardojaime86@gmail.com>
2023-02-23 20:30:50 -0500

    l07 demo files

---
+++ demos/lesson07/app.js
@@ -0,0 +1,73 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// import mongoose
+var mongoose = require('mongoose');
+// import configs
+const configs = require('./config/global');
+
+var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+var projectsRouter = require('./routes/projects');
+var coursesRouter = require('./routes/courses');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/projects', projectsRouter);
+app.use('/courses', coursesRouter);
+
+// configure connection, after routers
+mongoose.connect(configs.db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((message)=>{ console.log('Connected successfully!'); }) // successful connection
+  .catch((error)=>{ console.log('Error while connecting: ' + error); }); // error
+
+// HBS Helper Method to select values from dropdown lists
+const hbs = require('hbs');
+// function name and helper function with parameters
+hbs.registerHelper('createOption', (currentValue, selectedValue) => {
+  // initialize selected property
+  var selectedProperty = '';
+  // if values are equal set selectedProperty accordingly
+  if (currentValue == selectedValue) {
+    selectedProperty = 'selected';
+  }
+  // return html code for this option element
+  // return new hbs.SafeString('<option '+ selectedProperty +'>' + currentValue + '</option>');
+  return new hbs.SafeString(`<option ${selectedProperty}>${currentValue}</option>`);
+});
+
+hbs.registerHelper('toShortDate', (longDateValue) => {
+  return new hbs.SafeString(longDateValue.toLocaleDateString('en-CA'));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
