225eb37142a5f8262b4cd4ff892d87d0b0c755f3
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-16 20:42:53 -0400

    l09 part 3

--- demos/lesson09/my-app/src/app/project/project.component.ts
+++ demos/lesson09/my-app/src/app/project/project.component.ts
@@ -1,5 +1,20 @@
 // similar to a router object
 import { Component } from '@angular/core';
+
+// ideally you would connect to a server to retrieve data JSON
+export class Project {
+  id!: number; // add ! to skipping null check
+  name!: string;
+  course!: string;
+}
+// create mock list of project
+const PROJECTS: Project[] = [
+  {id: 101, name: 'Lab 1', course: 'JS Frameworks'},
+  {id: 102, name: 'Lab 2', course: 'JS Frameworks'},
+  {id: 103, name: 'Lab 3', course: 'JS Frameworks'},
+  {id: 104, name: 'Group Tutorials', course: 'JS Frameworks'}
+];
+
 // configuration section
 @Component({
   selector: 'app-project', // html element name
@@ -8,4 +23,5 @@ import { Component } from '@angular/core';
 })
 export class ProjectComponent {
   // add functionality and data here
-}
+  projects = PROJECTS; // attribute of my component
+} 
