9fad32e46bcaef501df0154d33791ba97bda29b9
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-23 20:22:32 -0400

    l10 part 2

---
+++ demos/lesson10/project-tracker-frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [ProjectsComponent] // projects is the starting point of my app
+})
+export class AppModule { }
