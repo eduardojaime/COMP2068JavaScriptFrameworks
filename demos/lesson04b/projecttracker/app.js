891d374d5005086573dd365dc9afb62c74d8875b
Eduardo Jaime <eduardojaime86@gmail.com>
2023-05-30 21:39:49 -0400

    l04 demo part 2

--- demos/lesson04b/projecttracker/app.js
+++ demos/lesson04b/projecttracker/app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// Router Objects
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -18,9 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Routing configurations
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +39,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// Exports all code in order to use it somewhere else
 module.exports = app;
--- demos/lesson04b/projecttracker/routes/index.js
+++ demos/lesson04b/projecttracker/routes/index.js
@@ -1,9 +1,19 @@
+// Controllers = routers
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// GET handler for // < root of the site
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// GET handler for //about
+// Considering About is a page part of Landing Section
+// router.get('/about', (req, res, next) => {
+//   // first param is view name
+//   // second is json object with data for the view
+//   res.render('about', { title: 'About Us'});
+// });
 
 module.exports = router;
