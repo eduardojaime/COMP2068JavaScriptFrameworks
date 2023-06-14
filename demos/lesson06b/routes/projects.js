b4804922c826be422a07ad6ed6790e90c6375c1e
Eduardo Jaime <eduardojaime86@gmail.com>
2023-06-13 21:47:25 -0400

    l06 in-class demo

--- demos/lesson06b/routes/projects.js
+++ demos/lesson06b/routes/projects.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 // import mongoose model in order to interact with the DB
 const Project = require("../models/project"); // .. navigates a folder up
+const Course = require("../models/course"); // to show course list
 // Configure router object with request handlers
 // GET handler for /Projects/
 router.get("/", (req, res, next) => {
@@ -23,19 +24,28 @@ router.get("/", (req, res, next) => {
   Project.find((err, projects) => {
     if (err) {
       console.log(err);
-    }
-    else {
+    } else {
       res.render("projects/index", {
-            title: "Project Tracker",
-            dataset: projects,
-          });
+        title: "Project Tracker",
+        dataset: projects,
+      });
     }
   });
 });
 
 // GET handler for /Projects/Add > shows empty form for users to fill in
 router.get("/add", (req, res, next) => {
-  res.render("projects/add", { title: "Add a New Project" });
+  // RETRIEVE ALL COURSES AND SEND TO VIEW
+  Course.find((err, courses) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("projects/add", {
+        title: "Add a New Project",
+        courseList: courses,
+      });
+    }
+  });
 });
 
 // POST handler for /Projects/Add > triggered when users click the SAVE button in the form
@@ -53,6 +63,24 @@ router.post("/add", (req, res, next) => {
         console.log(err);
       } else {
         res.redirect("/projects"); // success redirect to projects page
+      }
+    }
+  );
+});
+
+// GET handler for /projects/delete/{id}
+// example /projects/delete/6482781da707543bdb4c5d7f
+router.get("/delete/:_id", (req, res, next) => {
+  // _id can be accessed from req.params after being defined in the path above
+  Project.remove(
+    {
+      _id: req.params._id,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/projects");
       }
     }
   );
