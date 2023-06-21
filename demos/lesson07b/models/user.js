b32d22c930b8cff61c93d599135c039d0b4b8cda
Eduardo Jaime <eduardojaime86@gmail.com>
2023-06-20 20:54:52 -0400

    l07 part 2 configuring passport

---
+++ demos/lesson07b/models/user.js
@@ -0,0 +1,15 @@
+// mongoose model with passport related features
+// import mongoose
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
+// create schema obj
+const schemaObj = {
+  username: { type: String, required: true },
+  password: { type: String, required: true }, // Security Rule : Avoid storing plain text passwords!
+};
+// create mongoose schema
+const mongooseSchema = new mongoose.Schema(schemaObj);
+// Add plugins to the mongoose schema to enhance its features
+mongooseSchema.plugin(plm); // inject functionality contained in plm
+// create and export mongoose model
+module.exports = new mongoose.model("User", mongooseSchema);
