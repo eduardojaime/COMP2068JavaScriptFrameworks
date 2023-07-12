d4096b76997ff8c18f70a85abd13ae1cfd54b822
Eduardo Jaime <eduardojaime86@gmail.com>
2023-07-11 20:42:57 -0400

    l09 part 3

--- demos/lesson09b/my-app/src/app/projects/projects.component.ts
+++ demos/lesson09b/my-app/src/app/projects/projects.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+// Mock Project Object
+export class Project {
+  // property: type;
+  id!: number;
+  name!: string;
+  course!: string;
+}
+
+const PROJECTS: Project[] = [
+  { id: 1, name: 'Lab 1', course: 'JS Frameworks' },
+  { id: 2, name: 'Lab 2', course: 'JS Frameworks' },
+  { id: 3, name: 'Lab 3', course: 'JS Frameworks' }
+]
+
 @Component({
   selector: 'app-projects', // html element <app-projects></app-projects>
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
   // define properties
+  // ideally this would be a call to the service to return data
+  // project: Project = { id: 1, name: 'Lab 1', course: 'JS Frameworks' };
+  projects = PROJECTS; // this would be a call to a function that calls the Back End
   // define methods
 }
