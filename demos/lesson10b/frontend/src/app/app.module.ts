7f9db5d086ff16d6d4f6444360621f1fe8c23fb0
Eduardo Jaime <eduardojaime86@gmail.com>
2023-07-18 20:25:57 -0400

    l10 part 3 creating service class

--- demos/lesson10b/frontend/src/app/app.module.ts
+++ demos/lesson10b/frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
+// similar to app.js
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
+// Import the http client module to enable http requests
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent
   ],
-  imports: [
+  imports: [ // register modules globally
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, // now my angular classes will have access to a HttpClient object
   ],
   providers: [],
   bootstrap: [ProjectsComponent] // component that loads on app startup
