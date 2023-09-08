f7744d9ccaa96a0b1a6881deea1b8c54b45c3bda
Eduardo Jaime <eduardojaime86@gmail.com>
2023-09-07 21:40:23 -0400

    l01 hello world

---
+++ demos/lesson01c/helloworld.js
@@ -0,0 +1,4 @@
+// you can execute any JS code except code that is designed for browsers
+// for example: alert()
+// alert("hello!"); // will not work
+console.log("Hello World!"); // this will work and show text in the terminal below
