9f7de8858cfb9ec7ad1e19e8b57e2ca548053cc1
Eduardo Jaime <eduardojaime86@gmail.com>
2023-09-28 20:26:44 -0400

    l04 part 2 project template

--- demos/lesson04c/part1/server.js
+++ demos/lesson04c/part1/server.js
@@ -3,8 +3,15 @@ const express = require("express");
 // Create an app object
 const app = express();
 // Configure app object, add routes
+app.use((req, res, next) => {
+    console.log("Request received on: " + req.url);
+    next();
+});
 app.get("/", (req, res, next) => {
     res.send("Hello JS Frameworks!");
+});
+app.get("/about", (req, res, next) => {
+    res.send("About Us");
 });
 // Make app listen to a port
 app.listen(3000);
