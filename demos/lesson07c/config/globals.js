3ccc5922fb3c06a9a5d2d064fa01145ee048cd30
Eduardo Jaime <eduardojaime86@gmail.com>
2023-10-19 20:09:33 -0400

    l07 project template

---
+++ demos/lesson07c/config/globals.js
@@ -0,0 +1,7 @@
+// JSON object that contains global configuration values
+const configurations = {
+    // key : value pairs
+    "db": "mongodb+srv://projectrackerwebapp:TyMIbDdQIaywXi9P@cluster0.ho53tdt.mongodb.net/ProjectTrackerWebApp"
+}
+// export object so we can import it with require()
+module.exports = configurations;
