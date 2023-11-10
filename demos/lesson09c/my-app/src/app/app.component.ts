3e4125b5371d4e1cfefbc472c077cd2a2c0a0af0
Eduardo Jaime <eduardojaime86@gmail.com>
2023-11-09 20:45:26 -0500

    l09 part 2 data binding

--- demos/lesson09c/my-app/src/app/app.component.ts
+++ demos/lesson09c/my-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 // similar to index.js router object in Express
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // html tag that represents this component
+  templateUrl: './app.component.html', // view
+  styleUrls: ['./app.component.css'] // styles
 })
 export class AppComponent {
-  title = 'my-app';
+  // similar to C# or Java
+  // classes can have data and behaviour
+  title = 'Welcome to my Angular App!'; // properties that can be accessed in the view
+  logoImg = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
 }
