7f155933786fa79cfb9aecbfd8e9936eb71de0f7
Eduardo Jaime <eduardojaime86@gmail.com>
2023-11-13 20:02:58 -0500

    l10 part 1 server backend

--- demos/lesson10a/spa-backend/app.js
+++ demos/lesson10a/spa-backend/app.js
@@ -1,41 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var projectsRouter = require("./routes/projects")
+// Connecting to the DB
+var mongoose = require("mongoose");
+var configs = require("./config/globals");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/projects", projectsRouter);
+// connect to Mongo DB
+mongoose
+  .connect(configs.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((msg) => {
+    console.log("Success!");
+  })
+  .catch((err) => {
+    console.log("Error while connecting: " + err);
+  });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ demos/lesson10a/spa-backend/config/globals.js
@@ -0,0 +1,5 @@
+const configurations = {
+    db: 'mongodb+srv://webappdbuser:nkYWAR2AUgfWyH08@cluster0.ho53tdt.mongodb.net/ProjectTrackerWebApp'
+}
+
+module.exports = configurations;
