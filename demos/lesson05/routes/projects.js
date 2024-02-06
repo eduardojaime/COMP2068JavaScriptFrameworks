39770bd3882ad1654a1dbdeb75a9873149eb13e7
Eduardo Jaime <eduardojaime86@gmail.com>
2024-02-05 21:28:38 -0500

    l05 part 4 CREATE functionality

--- demos/lesson05/routes/projects.js
+++ demos/lesson05/routes/projects.js
@@ -2,11 +2,31 @@
 // Import express and create a router object
 const express = require("express");
 const router = express.Router();
+// Import Model for performing DB Operations
+const Project = require("../models/project"); // use .. to navigate one folder up
 // Configure GET/POST handlers
 // GET /Projects/
 router.get("/", (req, res, next) => {
     // relative to the views folder
     res.render("projects/index", { title: "Projects" });
 });
+
+// CREATE Functionality
+// GET /Projects/Add > Loading the page with the form
+router.get("/add", (req, res, next) => {
+    res.render("projects/add", { title: "Add a new Project" });
+});
+// POST /Projects/Add > When user fills in the form and clicks Save button
+router.post("/add", async (req, res, next) => {
+    // Use the model to create a new Project and save it in the DB
+    let newProject = new Project({
+        name: req.body.name, // req.body = form input fields
+        dueDate: req.body.dueDate,
+        course: req.body.course
+    });
+    await newProject.save();
+    res.redirect("/projects");
+});
+
 // Export router module
 module.exports = router;
