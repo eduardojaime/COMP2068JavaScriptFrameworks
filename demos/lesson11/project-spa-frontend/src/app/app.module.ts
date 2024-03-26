aad2b9cf1f44bfc230c67886823a0cf67abb1fe9
Eduardo Jaime <eduardojaime86@gmail.com>
2024-03-25 21:07:39 -0400

    l11 part 3 service layer

--- demos/lesson11/project-spa-frontend/src/app/app.module.ts
+++ demos/lesson11/project-spa-frontend/src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 
+// import HttpClientModule to make HTTP requests and ProjectService to call the REST backend API
+import { HttpClientModule } from '@angular/common/http';
+import { ProjectsService } from './services/projects.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent
   ],
-  imports: [
+  imports: [ // modules are components that enhance what the app can do
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProjectsService], // providers are services that the app module can use to retrieve data
   bootstrap: [ProjectsComponent]
 })
 export class AppModule { }
