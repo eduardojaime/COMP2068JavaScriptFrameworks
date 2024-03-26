9b545dc1b192c0c356d19642da6c66d58ad83699
Eduardo Jaime <eduardojaime86@gmail.com>
2024-03-25 20:04:32 -0400

    l11 part 1 READ projects

--- demos/lesson11/project-spa-backend/app.js
+++ demos/lesson11/project-spa-backend/app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
+var projectsRouter = require("./routes/projects");
 // Database Connectivity
 var mongoose = require("mongoose");
 var configs = require("./configs/globals");
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routing Configuration
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api/projects", projectsRouter);
 
 // Connect to MongoDB
 mongoose
