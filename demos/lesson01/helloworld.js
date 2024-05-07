9fe24d9887f9063aed601e52209a90cfde4de588
Eduardo Jaime <eduardojaime86@gmail.com>
2024-05-06 20:41:23 -0400

    archived w24 demos

--- demos/lesson01/helloworld.js
+++
@@ -1,4 +0,0 @@
-// Print hello to the terminal!
-console.log("Hello world!");
-// Keep in mind this code runs only in your computer, so browser-level functions are not available
-// alert("hello!"); // <<< will not show anything, breaks with error "Alert is not defined"
