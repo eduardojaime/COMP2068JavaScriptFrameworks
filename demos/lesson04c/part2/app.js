614ed45e439b62e5d04f1b31a71040fe82cc4025
Eduardo Jaime <eduardojaime86@gmail.com>
2024-05-30 20:36:05 -0400

    l04 part 2 modifications

--- demos/lesson04c/part2/app.js
+++ demos/lesson04c/part2/app.js
@@ -1,12 +1,14 @@
+// This file is used for configuring the app object
+// Configure global features: database connectivity, routing, middlewares, etc.
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// Import the router objects
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+// Create app object
 var app = express();
 
 // view engine setup
@@ -18,7 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Routing configuration
+// Associating a router object to a path
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +40,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// Export the app object to make it available in bin/www
 module.exports = app;
