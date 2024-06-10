a7aaed1ab0e59141971e6335437cb1da2cf30b1e
Eduardo Jaime <eduardojaime86@gmail.com>
2024-06-10 19:57:55 -0400

    l06 exercise solution

--- demos/lesson06a/app.js
+++ demos/lesson06a/app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 // var usersRouter = require('./routes/users');
 var projectsRouter = require("./routes/projects");
+var coursesRouter = require("./routes/courses");
 // Database Connectivity
 var mongoose = require("mongoose");
 var globals = require("./configs/globals"); // global variables
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 // app.use('/users', usersRouter);
 app.use("/projects", projectsRouter);
+app.use("/courses", coursesRouter);
 // Connect to MongoDB
 mongoose
   .connect(globals.ConnectionString.MongoDB)
