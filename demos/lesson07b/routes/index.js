3b5d270937528eb2f4d8b66b73871a1ea217c591
Eduardo Jaime <eduardojaime86@gmail.com>
2024-06-19 20:41:07 -0400

    l07 part 2

--- demos/lesson07b/routes/index.js
+++ demos/lesson07b/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Authentication functionality will be part of the index route (Home Page)
+
+// GET /login > load the login form
+// POST /login > when user clicks on the button
+// GET /register > load the registration form
+router.get("/register", (req, res, next) => {
+  res.render("register", { title: "Sign Up for an Account" });
+});
+// POST /register > when user clicks on the button
+router.post("/register", (req, res, next) => {
+  // Create a new user, register() is provided by passport-local-mongoose
+  User.register( 
+    new User({ username: req.body.username }), // new user object
+    req.body.password, // password as simple text to be encrypted
+    (err, newUser) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      }
+      else {
+        // log user in and redirect to projects
+        req.login(newUser, (err) => { res.redirect("/projects"); });
+      }
+    } // callback function
+  );
 });
 
 module.exports = router;
