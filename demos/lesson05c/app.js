98f9090307240f8480c700aaaae527abf395012b
Eduardo Jaime <eduardojaime86@gmail.com>
2024-10-03 21:11:45 -0400

    l05 part 2 connecting to mongodb from the app

--- demos/lesson05c/app.js
+++ demos/lesson05c/app.js
@@ -1,41 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+// Import mongoose and the configurations object
+var mongoose = require("mongoose");
+var configs = require("./configs/globals");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+// Middleware configuration
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
+// Route configuration
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+// Connect to MongoDB after the route configuration
+// Use method chaining
+mongoose
+  .connect(configs.ConnectionString.MongoDB)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error);
+  });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
