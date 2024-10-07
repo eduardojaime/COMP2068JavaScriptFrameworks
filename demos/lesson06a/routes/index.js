e75021360803002e2be2739ce97550dd1c04ba44
Eduardo Jaime <eduardojaime86@gmail.com>
2024-10-07 19:26:48 -0400

    l06 project template

--- demos/lesson06a/routes/index.js
+++ demos/lesson06a/routes/index.js
@@ -5,10 +5,6 @@ var Project = require('../models/project');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  // Get all projects by using the find method in the model object
-  let projects = await Project.find();
-  console.log(projects);
-  // then do something with the list
   res.render('index', { title: 'Express' });
 });
 
