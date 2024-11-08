8f1c5a6ce0e6e4c5c24ac3283b20c8739206d7be
Eduardo Jaime <eduardojaime86@gmail.com>
2024-11-07 20:15:38 -0500

    l09 part 2 data binding

--- demos/lesson09c/my-app/src/app/app.component.ts
+++ demos/lesson09c/my-app/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'My First Angular App'; // used in interpolation binding as {{ title }}
 }
