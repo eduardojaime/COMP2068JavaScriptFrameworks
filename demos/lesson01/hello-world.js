56f0c389d7fdf125b7e3415653aaf7a683a8f100
Eduardo Jaime <eduardojaime86@gmail.com>
2024-12-27 22:03:12 -0500

    L01 demo files

---
+++ demos/lesson01/hello-world.js
@@ -0,0 +1,6 @@
+// Hello World Console App with Node.js
+// Most syntax is the same as JavaScript for the browser
+console.log('Hello World!');
+// There are a few exceptions, such as alert() and prompt()
+alert('Hello World!'); // This will not work
+// breaks with error alert is not defined at Object.<anonymous>
