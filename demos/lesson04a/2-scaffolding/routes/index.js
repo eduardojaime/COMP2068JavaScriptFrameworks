249606ff1dfd266cafe19e9b0e69b904b47e72e3
Eduardo Jaime <eduardojaime86@gmail.com>
2025-01-27 21:15:11 -0500

    l04 scaffolding example

---
+++ demos/lesson04a/2-scaffolding/routes/index.js
@@ -0,0 +1,10 @@
+// Controllers or Router objects
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
