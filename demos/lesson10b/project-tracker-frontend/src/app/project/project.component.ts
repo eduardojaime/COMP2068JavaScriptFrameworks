e54d3da97271ee0627bd0c3550a30f298ab32e1a
Eduardo Jaime <eduardojaime86@gmail.com>
2025-03-18 20:29:55 -0400

    l10 generate service and component classes

---
+++ demos/lesson10b/project-tracker-frontend/src/app/project/project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  standalone: false,
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.css'
+})
+export class ProjectComponent {
+
+}
