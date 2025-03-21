35945d6ab54e4a502f30329c0b8993e02e882f8c
Eduardo Jaime <eduardojaime86@gmail.com>
2025-03-20 20:03:49 -0400

    l10 backend modifications

--- demos/lesson10c/project-tracker-backend/app.js
+++ demos/lesson10c/project-tracker-backend/app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var projectsRouter = require('./routes/projects');
+
+// Import configs object and mongoose package
+var configs = require('./configs/globals');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -18,9 +23,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Routing Configuration
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// Best Practice : have dedicated routes for API endpoints
+app.use('/api/projects', projectsRouter);
+// Connect to the DB
+mongoose.connect(configs.ConnectionStrings.MongoDB)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
