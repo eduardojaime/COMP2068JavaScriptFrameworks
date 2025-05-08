2a37332f907ef72e2e3fda8f0b86053f4ed96e70
Eduardo Jaime <eduardojaime86@gmail.com>
2025-05-07 20:29:58 -0400

    Archived demos

--- demos/lesson09b/my-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-// This is similar to bin/www in Express.js
-// It is the entry point of the Angular application
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
--- demos/lesson09c/my-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-// Similar to bin/www in Express.js
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
