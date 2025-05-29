7c7001723b8f588f8d97c2313345d672c25aa980
Eduardo Jaime <eduardojaime86@gmail.com>
2025-05-29 19:46:17 -0400

    l04 part 2 about page

--- demos/lesson04b/scaffolded-template/routes/index.js
+++ demos/lesson04b/scaffolded-template/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// These paths are relative to the path specified in the app.js file
+// GET //
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// // GET /about
+// router.get("/about", function(req, res, next) {
+//   res.render("about", { title: "About Us"});
+// });
 
 module.exports = router;
