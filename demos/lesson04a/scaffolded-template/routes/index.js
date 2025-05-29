8a1761b574321e1d5fbab62e5c401aca84b12d13
Eduardo Jaime <eduardojaime86@gmail.com>
2025-05-28 20:41:55 -0400

    l04 adding routes

--- demos/lesson04a/scaffolded-template/routes/index.js
+++ demos/lesson04a/scaffolded-template/routes/index.js
@@ -2,8 +2,28 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
+// GET // > Landing page for this section
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", data: data });
+  res.render("index", { title: "Express" });
 });
+
+// Option 1) About is part of Landing section
+// // More granular routes can be added here
+// // These are relative to the path defined in app.js
+// router.get("/about", function (req, res, next) {
+//   var toolsList = [
+//     { name: "C#" },
+//     { name: "JavaScript" },
+//     { name: "Python" },
+//     { name: "Java" },
+//     { name: "Ruby" },
+//     { name: "Go" },
+//     { name: "Rust" },
+//     { name: "PHP" },
+//     { name: "Swift" },
+//     { name: "Kotlin" }
+//   ]
+//   res.render("about", { title: "About Us", tools: toolsList });
+// });
 
 module.exports = router;
