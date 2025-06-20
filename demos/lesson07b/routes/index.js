52a5020a52eee81e306a5209fbeb9397ee2e0774
Eduardo Jaime <eduardojaime86@gmail.com>
2025-06-19 20:01:48 -0400

    l07 part 2 login and register pages

--- demos/lesson07b/routes/index.js
+++ demos/lesson07b/routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var User = require("../models/user");
+var passport = require("passport");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// GET /login
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Login" });
+});
+
+// POST /login
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/projects",
+    failureRedirect: "/login",
+    failureMessage: "Invalid Credentials",
+  })
+);
+
+// GET /register
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "Register" });
+});
+
+// POST /register
+router.post("/register", function (req, res, next) {
+  // Call the register method on the User model (plm)
+  // it will handle password hashing and user creation
+  // pass the new user object, password, and a callback function to handle the result
+  User.register(
+    new User({
+      username: req.body.username,
+    }),
+    req.body.password,
+    (error, newUser) => {
+      if (error) {
+        console.log("Error registering user:", error);
+        return res.redirect("/register");
+      } else {
+        req.login(newUser, (error) => {
+          res.redirect("/projects");
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
