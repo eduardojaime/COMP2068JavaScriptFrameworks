d7a21f1527f7c4bc329898601661b3b7eaf0d38e
Eduardo Jaime <eduardojaime86@gmail.com>
2025-07-16 19:34:04 -0400

    l10 frontend component

--- demos/lesson10a/project-tracker-frontend/src/app/app-module.ts
+++ demos/lesson10a/project-tracker-frontend/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Projects } from './projects/projects';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Projects
   ],
   imports: [
     BrowserModule,
---
+++ demos/lesson10a/project-tracker-frontend/src/app/projects/projects.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: false,
+  templateUrl: './projects.html',
+  styleUrl: './projects.css'
+})
+export class Projects {
+
+}
