30becc1044a898e6bf569a593c8078cfa58cbbf3
Eduardo Jaime <eduardojaime86@gmail.com>
2025-11-05 20:25:34 -0500

    l09 project template

---
+++ demos/lesson09b/my-app/src/app/app-module.ts
@@ -0,0 +1,21 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
