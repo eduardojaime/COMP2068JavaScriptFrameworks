20c2aa6d137a2ea40db695b1033c45fa621dbb72
Eduardo Jaime <eduardojaime86@gmail.com>
2025-11-05 20:44:26 -0500

    l09 overview and notes

--- demos/lesson09b/my-app/src/app/app.ts
+++ demos/lesson09b/my-app/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = signal('My Angular App');
+  // Add function to show an alert
+  protected showAlert() {
+    alert("Hello Everyone!");
+  }
 }
