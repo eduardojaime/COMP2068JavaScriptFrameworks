a32a6747343b7ac1516141e4dd6c2d8fc8cfa6cb
Eduardo Jaime <eduardojaime86@gmail.com>
2026-06-15 21:50:21 -0400

    L07 added update and delete

---
+++ demos/lesson07/public/javascripts/scripts.js
@@ -0,0 +1,3 @@
+function confirmDelete() {
+    return confirm("Are you sure you want to delete this item?");
+}
--- demos/lesson07/routes/projects.js
+++ demos/lesson07/routes/projects.js
@@ -30,10 +30,26 @@ router.post("/new", async (req, res, next) => {
 });
 
 // UPDATE - GET /projects/edit/:id > shows a form with the project data to edit
+router.get("/edit/:id", async (req, res, next) => {
+  const project = await Project.findById(req.params.id);
+  res.render("projects/edit", { title: "Edit Project", project: project });
+});
 
 // UPDATE - POST /projects/edit/:id > receives form data and updates project in database
+router.post("/edit/:id", async (req, res, next) => {
+  const project = await Project.findById(req.params.id);
+  project.name = req.body.name;
+  project.dueDate = req.body.dueDate;
+  project.status = req.body.status;
+  await project.save();
+  res.redirect("/projects");
+});
 
 // DELETE - GET /projects/delete/:id > deletes project from database and redirects to /projects
+router.get("/delete/:id", async (req, res, next) => {
+  await Project.findByIdAndDelete(req.params.id);
+  res.redirect("/projects");
+});
 
 // Export the router object so it can be mounted on a path in app.js
 module.exports = router;
