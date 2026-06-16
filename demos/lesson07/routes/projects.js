2d72c66dfad2814ca34a9dc59d0b6020a723deae
Eduardo Jaime <eduardojaime86@gmail.com>
2026-06-15 21:39:16 -0400

    l07 added model and read & create operations

--- demos/lesson07/routes/projects.js
+++ demos/lesson07/routes/projects.js
@@ -2,24 +2,32 @@
 // Import express and create a router object
 const express = require("express");
 const router = express.Router();
+// Import the project model to interact with the database
+const Project = require("../models/project");
 
 // Configure routes
 // All paths in this file are relative to the configured path in app.js
 // READ - GET /projects > shows a table with all projects in the database
-router.get("/", (req, res, next) => {
-  // TODO - get all projects from database and pass to view
-  const projects = [
-    {
-      name: "Assignment 01",
-      dueDate: "2026-06-30",
-      status: "TO DO",
-    },
-  ];
-  res.render("projects/index", { title: "All Projects", dataset: projects });
+router.get("/", async (req, res, next) => {
+  // Retrieve all projects from database (no filter) and pass them to the view
+  const projects = await Project.find();
+  console.log(projects);
+  res.render("projects/index", { title: "All Projects", dataset: projects });``
 });
 // CREATE - GET /projects/new > shows an empty form to create a new project
+router.get("/new", (req, res, next) => {
+  res.render("projects/new", { title: "Create New Project" });
+});
 
 // CREATE - POST /projects/new > receives from data and creates project in database
+router.post("/new", async (req, res, next) => {
+  const newProject = new Project({
+    name: req.body.name,
+    dueDate: req.body.dueDate
+  });
+  await newProject.save();
+  res.redirect("/projects");
+});
 
 // UPDATE - GET /projects/edit/:id > shows a form with the project data to edit
 
