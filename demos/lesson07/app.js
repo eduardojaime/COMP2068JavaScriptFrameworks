3a1db0854c0129ffe441a371a559f65f3875fff3
Eduardo Jaime <eduardojaime86@gmail.com>
2026-06-15 20:01:26 -0400

    l07 setting up a new route

--- demos/lesson07/app.js
+++ demos/lesson07/app.js
@@ -1,12 +1,13 @@
+// Equivalent to Program.cs
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// Load router object
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+// App object creation
 var app = express();
 
 // view engine setup
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Routing Tables
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +38,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// Export the app object so it can be mounted on a server
 module.exports = app;
--- demos/lesson07/routes/index.js
+++ demos/lesson07/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// All paths in this file are relative to the configured path in app.js
+// GET /about
+router.get("/about", (req, res, next) => {
+  // any processing logic goes here
+  // render a view, standard is to name it the same as the path
+  res.render("about", { title: "About Us" });
+});
+
 module.exports = router;
